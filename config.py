675d8d24194b347095579fea4e1dd3ede6b3b4ee
amanvishwak <147085734+amanvishwak@users.noreply.github.com>
2024-07-14 18:44:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ DB_NAME = environ.get("DB_NAME", "Cluster0")
 # Bot Information
 BOT_TOKEN = environ.get("BOT_TOKEN", "7232006758:AAGV-HPIfJi3k-gz2_YR6_-1k7706NEPD-I")
 BOT_USERNAME = environ.get("BOT_USERNAME", "AV_ADVANCE_BOT") # your bot username without @
-PICS = (environ.get('PICS', 'https://graph.org/file/82ef767ffebe3a948e476.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://graph.org/file/82ef767ffebe3a948e476.jpg https://graph.org/file/82ef767ffebe3a948e476.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
 AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
